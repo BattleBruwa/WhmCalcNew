06d4b48ba5b4c590144c3b55381b2bc04d76c3d4
Alex B <dop.bass-spb@mail.ru>
2024-04-23 15:58:54 +0300

    some changes

--- WhmCalcNew/Engine/Calculations/ToWoundCalc.cs
+++ WhmCalcNew/Engine/Calculations/ToWoundCalc.cs
@@ -37,14 +37,48 @@ namespace WhmCalcNew.Engine.Calculations
                 resultedRoll = 6;
             }
 
-            if (attacker.IsMinusOneToWound == true)
+
+            if (attacker.HasRerollToWound1 == false && attacker.HasRerollToWoundAll == false)
             {
-                return DiceRoller.RollTheDice(resultedRoll - 1);
+                if (attacker.IsMinusOneToWound == true)
+                {
+                    return DiceRoller.RollTheDice(resultedRoll + 1);
+                }
+                else if (attacker.IsMinusOneToWound == false)
+                {
+                    return DiceRoller.RollTheDice(resultedRoll);
+                }
             }
             else
             {
-                return DiceRoller.RollTheDice(resultedRoll);
+                if (attacker.HasRerollToWound1 == true)
+                {
+                    // Минус 1 ту вунд
+                    if (attacker.IsMinusOneToWound == true)
+                    {
+                        return DiceRoller.RollTheDiceWithReroll1s(resultedRoll + 1);
+                    }
+                    else if (attacker.IsMinusOneToWound == false)
+                    {
+                        return DiceRoller.RollTheDiceWithReroll1s(resultedRoll);
+                    }
+                }
+
+                if (attacker.HasRerollToWoundAll == true)
+                {
+                    // Минус 1 ту вунд
+                    if (attacker.IsMinusOneToWound == true)
+                    {
+                        return DiceRoller.RollTheDiceWithReroll(resultedRoll + 1);
+                    }
+                    else if (attacker.IsMinusOneToWound == false)
+                    {
+                        return DiceRoller.RollTheDiceWithReroll(resultedRoll);
+                    }
+                } 
             }
+
+            return DiceRoller.RollTheDice(resultedRoll);
         }
     }
 }
