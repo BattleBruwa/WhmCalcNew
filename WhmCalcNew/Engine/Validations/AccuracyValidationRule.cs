87bb32fe3dbaba0f96e1a71b065c1365394a6801
Alex B <dop.bass-spb@mail.ru>
2024-05-02 17:44:46 +0300

    some changes

---
+++ WhmCalcNew/Engine/Validations/AccuracyValidationRule.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace WhmCalcNew.Engine.Validations
+{
+    public class AccuracyValidationRule : ValidationRule
+    {
+        private const string _wsPattern = @"^([0-9]|[1-2]{1}[0-9]{1})$";
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            Regex regex = new Regex(_wsPattern);
+            string? input = value.ToString();
+
+            if (regex.IsMatch(input) == false)
+            {
+                return new ValidationResult(false, "Некорректное число");
+            }
+            else
+            {
+                return ValidationResult.ValidResult;
+            }
+        }
+    }
+}
+        
--- WhmCalcNew/Engine/Validations/StrenghtValidationRule.cs
+++ WhmCalcNew/Engine/Validations/StrenghtValidationRule.cs
@@ -4,12 +4,12 @@ using System.Windows.Controls;
 
 namespace WhmCalcNew.Engine.Validations
 {
-    public class DigitInputValidationRule : ValidationRule
+    public class StrenghtValidationRule : ValidationRule
     {
-        private const string _digitPattern = @"^([0-9]|[1-2]{1}[0-9]{1})$";
+        private const string _strPattern = @"^([0-9]|[1-2]{1}[0-9]{1})$";
         public override ValidationResult Validate(object value, CultureInfo cultureInfo)
         {
-            Regex regex = new Regex(_digitPattern);
+            Regex regex = new Regex(_strPattern);
             string? input = value.ToString();
 
             if (regex.IsMatch(input) == false)
