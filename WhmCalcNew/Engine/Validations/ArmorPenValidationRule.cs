4b7e3fbf436521266a54f9e0640cec2d99313cde
Alex B <dop.bass-spb@mail.ru>
2024-05-04 22:25:29 +0300

    some changes

---
+++ WhmCalcNew/Engine/Validations/ArmorPenValidationRule.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace WhmCalcNew.Engine.Validations
+{
+    public class ArmorPenValidationRule : ValidationRule
+    {
+        private const string _armorPenPattern = @"^([0-9]|[1-2]{1}[0-9]{1})$";
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            Regex regex = new Regex(_armorPenPattern);
+            string? input = value.ToString();
+
+            if (regex.IsMatch(input) == false)
+            {
+                return new ValidationResult(false, "Некорректное число");
+            }
+            else
+            {
+                return ValidationResult.ValidResult;
+            }
+        }
+    }
+}
---
+++ WhmCalcNew/Engine/Validations/AttacksDamageValidationRule.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace WhmCalcNew.Engine.Validations
+{
+    public class AttacksDamageValidationRule : ValidationRule
+    {
+        private const string _attacksOrDamagePattern = @"^([0-9]|[1-2]{1}[0-9]{1})$";
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            Regex regex = new Regex(_attacksOrDamagePattern);
+            string? input = value.ToString();
+
+            if (regex.IsMatch(input) == false)
+            {
+                return new ValidationResult(false, "Некорректное число");
+            }
+            else
+            {
+                return ValidationResult.ValidResult;
+            }
+        }
+    }
+}
