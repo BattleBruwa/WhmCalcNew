b8af197c9ec5bea66784d2d72a4e5f8e00a31eba
Alex B <dop.bass-spb@mail.ru>
2024-10-29 23:32:58 +0300

    some changes

--- WhmCalcNew/Services/DataAccess/DataContext.cs
+++ WhmCalcNew/Services/DataAccess/DataContext.cs
@@ -1,13 +1,10 @@
-﻿using System.IO;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using WhmCalcNew.Models;
 
 namespace WhmCalcNew.Services.DataAccess
 {
     public class DataContext: DbContext
     {
-        private readonly string dbPath = string.Concat(Path.GetFullPath("../../../Data"), "\\WhmUnitStatsDb.db3");
-
         public DbSet<TargetUnit> Targets { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
